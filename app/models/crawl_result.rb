121044375d9cbddd77721fca756d151c18651e7b
Anh Tran <yorkittran@gmail.com>
2023-03-14 23:55:21 +0700

    feat: config annotate

--- app/models/crawl_result.rb
+++ app/models/crawl_result.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: crawl_results
+#
+#  id            :bigint           not null, primary key
+#  search_time   :float            default(0.0), not null
+#  source        :text
+#  total_ads     :integer          default(0), not null
+#  total_links   :integer          default(0), not null
+#  total_results :integer          default(0), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :bigint           not null
+#
+# Indexes
+#
+#  index_crawl_results_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class CrawlResult < ApplicationRecord
   belongs_to :user
 end
