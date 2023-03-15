7aae0028f530947510d222251766c1f89d739fcc
Anh Tran <yorkittran@gmail.com>
2023-03-15 21:38:03 +0700

    fix: rubocop

--- app/services/scraper/search_service.rb
+++ app/services/scraper/search_service.rb
@@ -1,6 +1,7 @@
 module Scraper
   class SearchService < ApplicationService
     GOOGLE_SEARCH_URL = 'https://www.google.com.vn/search?q='.freeze
+    IMPORT_CRAWL_RESULT_COLUMNS = %i[user_id keyword total_results search_time total_links total_ads source].freeze
 
     def initialize(csv_file, current_user_id)
       @csv_file = csv_file
@@ -16,15 +17,14 @@ module Scraper
       end
       browser.close
 
-      columns = [:user_id, :keyword, :total_results, :search_time, :total_links, :total_ads]
-      CrawlResult.import(columns, crawl_results)
+      CrawlResult.import(IMPORT_CRAWL_RESULT_COLUMNS, crawl_results)
     end
 
   private
 
     attr_reader :csv_file, :current_user_id, :browser, :crawl_results
 
-    def crawl(keyword)
+    def crawl(keyword) # rubocop:disable Metrics/AbcSize
       data = {}
       doc = Nokogiri::HTML.parse(browser.html)
       stats = browser.element(id: 'result-stats').text.split
@@ -34,7 +34,7 @@ module Scraper
       data[:search_time] = stats[4][1..].tr(',', '.').to_f
       data[:total_links] = doc.xpath('//a/@href').count
       data[:total_ads] = doc.xpath("//div[@aria-label='Quảng cáo']/div").count
-      # data[:source] = browser.html
+      data[:source] = browser.html
       crawl_results << data
     end
   end
