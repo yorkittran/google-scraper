948d6344f9363e36fbd19d23f1642836d3e45fe6
Anh Tran <yorkittran@gmail.com>
2023-03-16 14:23:12 +0700

    feat: works with proxies

--- app/services/scraper/search_service.rb
+++ app/services/scraper/search_service.rb
@@ -1,41 +1,54 @@
+require 'csv'
+
 module Scraper
   class SearchService < ApplicationService
-    GOOGLE_SEARCH_URL = 'https://www.google.com.vn/search?q='.freeze
+    GOOGLE_SEARCH_URL = 'https://www.google.com.vn/search?gl=us&q='.freeze
     IMPORT_CRAWL_RESULT_COLUMNS = %i[user_id keyword total_results search_time total_links total_ads source].freeze
 
     def initialize(csv_file, current_user_id)
       @csv_file = csv_file
       @current_user_id = current_user_id
-      @browser = Watir::Browser.new(:chrome, headless: true)
       @crawl_results = []
     end
 
     def call
-      CSV.foreach(csv_file) do |keyword|
+      total_proxies = Settings.proxy.proxies.count
+      CSV.foreach(csv_file).with_index do |keyword, index|
+        browser = Watir::Browser.new(:edge, headless: true, proxy: proxy(index % total_proxies))
+        puts "#{index} - #{Settings.proxy.proxies[index % total_proxies]}"
+        puts "#{GOOGLE_SEARCH_URL}#{keyword.first}"
+
         browser.goto("#{GOOGLE_SEARCH_URL}#{keyword.first}")
-        crawl(keyword.first)
+        stats = browser.element(id: 'result-stats').text.split
+        crawl_results << crawl_data(keyword.first, browser.html)
+        browser.quit
       end
-      browser.close
 
       CrawlResult.import(IMPORT_CRAWL_RESULT_COLUMNS, crawl_results)
     end
 
   private
 
-    attr_reader :csv_file, :current_user_id, :browser, :crawl_results
+    attr_reader :csv_file, :current_user_id, :crawl_results
 
-    def crawl(keyword) # rubocop:disable Metrics/AbcSize
-      data = {}
-      doc = Nokogiri::HTML.parse(browser.html)
-      stats = browser.element(id: 'result-stats').text.split
-      data[:user_id] = current_user_id
-      data[:keyword] = keyword
-      data[:total_results] = stats[1].delete('.').to_i
-      data[:search_time] = stats[4][1..].tr(',', '.').to_f
-      data[:total_links] = doc.xpath('//a/@href').count
-      data[:total_ads] = doc.xpath("//div[@aria-label='Quảng cáo']/div").count
-      data[:source] = browser.html
-      crawl_results << data
+    def crawl_data(keyword, html)
+      doc = Nokogiri::HTML.parse(html)
+      {
+        user_id: current_user_id,
+        keyword: keyword,
+        total_results: doc.xpath("//div[@id='result-stats']/text()").text.delete('^0-9').to_i,
+        search_time: doc.xpath("//div[@id='result-stats']/nobr/text()").text.delete('^0-9.').to_f,
+        total_links: doc.xpath('//a/@href').count,
+        total_ads: doc.xpath("//div[@aria-label='Ads']/div").count,
+        source: html,
+      }
+    end
+
+    def proxy(index)
+      {
+        http: "#{Settings.proxy.username}:#{Settings.proxy.password}@#{Settings.proxy.proxies[index]}",
+        ssl: "#{Settings.proxy.username}:#{Settings.proxy.password}@#{Settings.proxy.proxies[index]}",
+      }
     end
   end
 end
