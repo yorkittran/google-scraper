167358b0ff05d53374a3ed0fa20fd34868b3aeec
Anh Tran <yorkittran@gmail.com>
2023-03-20 19:56:03 +0700

    feat: add read status in notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,6 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  content    :string           default(""), not null
+#  status     :integer          default(0), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :bigint           not null
@@ -18,4 +19,13 @@
 #
 class Notification < ApplicationRecord
   belongs_to :user
+
+  module Status
+    ALL = %i[unread read].freeze
+
+    unread = 'unread'.freeze
+    read = 'active'.freeze
+  end
+
+  enum status: Status::ALL
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_123703) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_125243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_123703) do
     t.string "content", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0, null: false
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
